ae4e566d91413763af14c743cc9a57dee790c24a
jsakamoto <junichi.jp@gmail.com>
2015-11-22 14:21:37 +0900

    Enhance NuGet package installation: Install as a Hosting Server.

---
+++ OwinHostForHeroku/install.ps1
@@ -0,0 +1,31 @@
+﻿param($installPath, $toolsPath, $package, $project)
+$serverProvider = $dte.GetObject("CustomWebServerProvider")
+if ($serverProvider -eq $null)
+{
+    return; # Only supported on VS 2013
+}
+$servers = $serverProvider.GetCustomServers($project.Name)
+if ($servers -eq $null)
+{
+    return; # Not a WAP project
+}
+$solutionDir = [System.IO.Path]::Combine($installPath, "..\..\")
+$solutionDir = [System.IO.Path]::GetFullPath($solutionDir)
+$relativeToolsDir = $toolsPath.SubString($solutionDir.Length)
+$exeDir = '{solutiondir}\' + $relativeToolsDir + '\OwinHostForHeroku.exe'
+$server = $servers.GetWebServer('OwinHost for Heroku')
+if ($server -ne $null)
+{
+    $servers.UpdateWebServer('OwinHost for Heroku', $exeDir, $server.CommandLine, $server.Url, $server.WorkingDirectory)
+}
+else
+{
+    try
+    {
+       $servers.AddWebServer('OwinHost for Heroku', $exeDir, '{url}', 'http://localhost:12345/', '{projectdir}')
+    }
+    catch [System.OperationCanceledException]
+    {
+        # The user hit No when prompted about locking the VS version.
+    }
+}
---
+++ OwinHostForHeroku/uninstall.ps1
@@ -0,0 +1,21 @@
+﻿param($installPath, $toolsPath, $package, $project)
+$serverProvider = $dte.GetObject("CustomWebServerProvider")
+if ($serverProvider -eq $null)
+{
+    return; # Only supported on VS 2013
+}
+$servers = $serverProvider.GetCustomServers($project.Name)
+if ($servers -eq $null)
+{
+    return; # Not a WAP project
+}
+$server = $servers.GetWebServer('OwinHost for Heroku')
+if ($server -ne $null)
+{
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    $prompt = [Microsoft.VisualBasic.Interaction]::MsgBox("Remove custom server settings?",'YesNo,Question', "Uninstalling Custom Server")
+    if ($prompt -eq [Microsoft.VisualBasic.MsgBoxResult]::Yes)
+    {
+        $servers.RemoveWebServer('OwinHost fro Heroku');
+    }
+}
