431a0ad184cfa5669a5b81d6d8ab7957b2e105c9
jsakamoto <junichi.jp@gmail.com>
2015-11-24 22:49:42 +0900

    Change the way of bundling Owin host binary to include it to project as linked item.

--- OwinHostForHeroku/NuGet Package Source/tools/install.ps1
+++ OwinHostForHeroku/NuGet Package Source/tools/install.ps1
@@ -20,6 +20,41 @@ if ((Test-Path -PathType Leaf $destinationProcfilePath) -eq $false) {
 	}
 }
 
+# Add "OwinHostForHeroku.exe" as linked project item.
+$owinHostName = "OwinHostForHeroku"
+$owinHostFile = "$owinHostName.exe"
+$markerFile = "README-$owinHostName.txt"
+$owinHostPath = Join-Path $toolsPath $owinHostFile
+$projectUri = [uri]$project.FullName
+$owinHostUri = [uri]$owinHostPath
+$owinHostRelativePath = $projectUri.MakeRelative($owinHostUri) -replace "/","\"
+
+#   delete marker file.
+$project.ProjectItems.Item($markerFile).Delete()
+
+#   Add content item as a link that is Owin host binary saved at higher folder.
+#   (This is normal case.)
+if ($owinHostRelativePath -like "..\*") {
+	$project.ProjectItems.AddFromFile($owinHostPath)
+	$project.ProjectItems.Item($owinHostFile).Properties.Item("CopyToOutputDirectory").Value = 2
+	$project.Save()
+}
+
+#   Add content item as a link that is Owin host binary saved at lower folder.
+#   (This is the case that .csproj file and packages folder are in a same folder.)
+if ($owinHostRelativePath -notlike "..\*") {
+	# Treat the project file as a MSBuild script xml instead of DTEnv object model.
+	Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+	$projectXml = ([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | select -First 1).Xml
+
+	$itemGrp = $projectXml.CreateItemGroupElement()
+	$projectXml.AppendChild($itemGrp)
+	$item = $itemGrp.AddItem("Content", $owinHostRelativePath)
+	$item.AddMetadata("Link", $owinHostFile)
+	$item.AddMetadata("CopyToOutputDirectory", "PreserveNewest")
+	$project.Save()
+}
+
 # Install custom Web server provider.
 $serverProvider = $dte.GetObject("CustomWebServerProvider")
 if ($serverProvider -eq $null)
--- OwinHostForHeroku/NuGet Package Source/tools/uninstall.ps1
+++ OwinHostForHeroku/NuGet Package Source/tools/uninstall.ps1
@@ -25,6 +25,42 @@ if ((Test-Path -PathType Leaf $destinationProcfilePath) -eq $true) {
 	}
 }
 
+# Remove "OwinHostForHeroku.exe" from project item.
+$owinHostName = "OwinHostForHeroku"
+$owinHostFile = "$owinHostName.exe"
+$owinHostPath = Join-Path $toolsPath $owinHostFile
+$projectUri = [uri]$project.FullName
+$owinHostUri = [uri]$owinHostPath
+$owinHostRelativePath = $projectUri.MakeRelative($owinHostUri) -replace "/","\"
+
+#   Delete content item that is Owin host binary saved at higher folder.
+#   (This is normal case.)
+if ($owinHostRelativePath -like "..\*") {
+	$project.ProjectItems.Item($owinHostFile).Delete()
+	$project.Save()
+}
+
+#   Delete content item that is Owin host binary saved at lower folder.
+#   (This is the case that .csproj file and packages folder are in a same folder.)
+if ($owinHostRelativePath -notlike "..\*") {
+
+	# Treat the project file as a MSBuild script xml instead of DTEnv object model.
+	Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+	$projectXml = ([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | select -First 1).Xml
+
+	$projectXml.Children | `
+	where { $_ -is [Microsoft.Build.Construction.ProjectItemGroupElement] } | `
+	foreach { $_.Children } | `
+	where { ($_.Children | where {$_.Name -eq "Link" -and $_.Value -eq $owinHostFile}) -ne $null } | `
+	foreach {
+		$itemGrp = $_.Parent
+		$itemGrp.RemoveChild($_)
+		if ($itemGrp.Children.Count -eq 0) { $projectXml.RemoveChild($itemGrp) }
+	}
+	
+	$project.Save()
+}
+
 # Uninstall custom Web server provider.
 $serverProvider = $dte.GetObject("CustomWebServerProvider")
 if ($serverProvider -eq $null)
